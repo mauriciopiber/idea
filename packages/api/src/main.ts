6a521ca74d0294d65c06e8bac7061632e49fe9f9
Mauricio Piber <mauriciopiber@gmail.com>
2024-08-18 12:48:28 -0300

    feat: added express opt in to server

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.listen(3000);
 }
+
 bootstrap();
